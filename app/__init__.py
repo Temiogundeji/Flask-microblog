a73963b54d48020611529b23dca95c6ae645bcbb
Temiogundeji <yusuffogundeji@gmail.com>
2019-12-22 15:04:55 +0100

    login implemented and sqlalchemy instslled

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from config import Config
+# from Flask_boostrap import Bootstrap
 app= Flask(__name__)
-from app import routes
+# bootstrap = Bootstrap(app)
+app.config.from_object(Config)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+from app import routes
